fbb9087d46fd7fe1fe7e2c86842a2ca222f7842f
edwardsila <edwardsila000@gmail.com>
2024-01-08 11:53:05 +0000

    implement class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Module 6-base_geometry
+Defines a class BaseGeometry with an unimplemented area method
+"""
+
+
+class BaseGeometry:
+    """ class with area method """
+    def area(self):
+        """ Raises expection """
+        raise Exception("area() is not implemented")
