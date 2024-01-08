6bd62fecca83714a08fea440346164e75ad1b1c8
edwardsila <edwardsila000@gmail.com>
2024-01-08 12:14:50 +0000

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+module 10-square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """inherits from Rectangle, who inherits from BaseGeometry
+    methods:
+        __init__(self, size)
+    """
+    def __init__(self, size):
+        """initializes size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
