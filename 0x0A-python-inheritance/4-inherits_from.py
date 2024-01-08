29b50e78722e1d47a7f1ee8b0aa55f2dabba4c6b
edwardsila <edwardsila000@gmail.com>
2024-01-08 11:40:14 +0000

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ from a specific class
 
 def inherits_from(obj, a_class):
     """Returns True if obj is an instance inherited from a_class"""
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
