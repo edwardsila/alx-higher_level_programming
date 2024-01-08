8a7c7142b1bb231a68388f2d307533f25be5aa07
edwardsila <edwardsila000@gmail.com>
2024-01-08 11:32:16 +0000

    instance of inheritance

---
+++ 0x0A-python-inheritance/3-si_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Module 3-is_kind_of_class
+Checks for instance of a class or instance
+of a class inherited from
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ checks object if is an instance of its inheritance """
+    return isinstance(obj, a_class)
