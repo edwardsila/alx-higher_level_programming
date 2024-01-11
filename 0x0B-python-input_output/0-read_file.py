309ce0b4c3e1485959d59b4171539ff5887e8195
edwardsila <edwardsila000@gmail.com>
2024-01-11 15:11:56 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+module 0-read_file
+opens a file in read mode
+
+"""
+
+
+def read_file(filename=""):
+    """ opens a file """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read())
